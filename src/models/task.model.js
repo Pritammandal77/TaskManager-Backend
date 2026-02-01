79da5ae0bff7f0b4a4f37528a0160d53b51c437b
Pritam Mandal <pritampmandal@gmail.com>
2026-02-01 10:28:07 +0530

    created task schema

---
+++ src/models/task.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const taskSchema = new Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        status: {
+            type: String,
+            default: "pending",
+            enum: ["pending", "completed"]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Task = mongoose.model("Task", taskSchema)
